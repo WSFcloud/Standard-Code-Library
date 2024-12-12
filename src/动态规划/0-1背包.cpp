ffb6807909ff76b4df6258a01154caab08691b6a
WSFcloud <byhtdcc@gmail.com>
2024-12-12 12:07:57 +0800

    feat: 增加0-1背包

---
+++ src/动态规划/0-1背包.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+using namespace std;
+int N = 10000, V = 500;
+
+void dp(vector<int> f, vector<int> c, vector<int> w) {
+    for (int i = 1; i <= N; i++) {
+        for (int j = V; j >= c[i]; j--) {
+            f[j] = max(f[j], f[j - c[i]] + w[i]);
+        }
+    }
+}
