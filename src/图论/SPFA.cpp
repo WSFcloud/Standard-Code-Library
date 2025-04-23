48c5e923f99dad9f874156d961d59fc3fa6247be
WSFcloud <byhtdcc@gmail.com>
2025-04-23 16:57:44 +0800

    fix: 修正SPFA代码

--- src/图论/SPFA.cpp
+++ src/图论/SPFA.cpp
@@ -2,19 +2,18 @@
 #include <queue>
 #include <climits>
 using namespace std;
+
 const int INF = INT_MAX;
-vector<int> dis, vis, cnt; // cnt数组记录节点的进队次数，用于检测负环
-queue<int> q;
-int n, m, s;
+int n, m, s = 1;
 struct edge {
     int to, weight;
 };
-bool spfa(vector<vector<edge>> &graph) {
-    dis.assign(n + 1, INF);
-    vis.assign(n + 1, 0);
-    cnt.assign(n + 1, 0);
+bool spfa(vector<vector<edge>> graph) {
+    // cnt数组记录节点的进队次数，用于检测负环
+    vector<int> dis(n + 1, INF), vis(n + 1, 0), cnt(n + 1, 0);
     dis[s] = 0;
     vis[s] = 1;
+    queue<int> q;
     q.push(s);
     while (!q.empty()) {
         int u = q.front();
@@ -27,7 +26,7 @@ bool spfa(vector<vector<edge>> &graph) {
             if (dis[u] + w < dis[v]) {
                 dis[v] = dis[u] + w;
                 cnt[v] = cnt[u] + 1;
-                if (cnt[v] > n - 1) {
+                if (cnt[v] >= n) {
                     return false; // 负环存在，返回false
                 }
                 if (!vis[v]) {
