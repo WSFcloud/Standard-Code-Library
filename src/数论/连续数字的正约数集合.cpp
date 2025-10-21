5e0c1619843dff5a0b6e44461e7d16214794ee75
WSFcloud <byhtdcc@gmail.com>
2025-10-21 19:31:27 +0800

    fix: 修改连续数字的正约数集合代码风格

--- src/数论/连续数字的正约数集合.cpp
+++ src/数论/连续数字的正约数集合.cpp
@@ -1,8 +1,9 @@
-vector<vector<int>> f(n + 1);
-void divide(int n) {
+auto divide(int n) {
+    vector<vector<int>> f(n + 1);
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n / i; j++) {
             f[i * j].push_back(i);
         }
     }
+    return f;
 }
