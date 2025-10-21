317793bab2b667b4dab7aee02dd6f58d005e2d59
WSFcloud <byhtdcc@gmail.com>
2025-10-21 19:02:03 +0800

    fix: 修改FWT代码风格

--- src/数学/FWT.cpp
+++ src/数学/FWT.cpp
@@ -1,45 +1,49 @@
-// 按位或版本
-void FWT_or(int *A, int n, int t) {
-    for (int k = 2; k <= n; k *= 2) {
-        for (int i = 0; i < n; i += k) {
-            for (int j = 0; j < k / 2; j++) {
-                if (t > 0)
-                    A[i + j + k / 2] = (A[i + j + k / 2] + A[i + j]) % p;
-                else
-                    A[i + j + k / 2] = (A[i + j + k / 2] - A[i + j] + p) % p;
-            }
-        }
-    }
-}
-// 按位与版本
-void FWT_and(int *A, int n, int t) {
+// 按位或
+void FWT_or(vector<int> &a, int t, int p) {
+    int n = a.size();
     for (int k = 2; k <= n; k *= 2) {
         for (int i = 0; i < n; i += k) {
             for (int j = 0; j < k / 2; j++) {
                 if (t > 0) {
-                    A[i + j] = (A[i + j] + A[i + j + k / 2]) % p;
+                    a[i + j + k / 2] = (a[i + j + k / 2] + a[i + j]) % p;
                 } else {
-                    A[i + j] = (A[i + j] - A[i + j + k / 2] + p) % p;
+                    a[i + j + k / 2] = (a[i + j + k / 2] - a[i + j] + p) % p;
                 }
             }
         }
     }
 }
-// 按位异或版本
-void FWT_xor(int *A, int n, int t) {
+// 按位与
+void FWT_and(vector<int> &a, int t, int p) {
+    int n = a.size();
     for (int k = 2; k <= n; k *= 2) {
         for (int i = 0; i < n; i += k) {
             for (int j = 0; j < k / 2; j++) {
-                int a = A[i + j], b = A[i + j + k / 2];
-                A[i + j] = (a + b) % p;
-                A[i + j + k / 2] = (a - b + p) % p;
+                if (t > 0) {
+                    a[i + j] = (a[i + j] + a[i + j + k / 2]) % p;
+                } else {
+                    a[i + j] = (a[i + j] - a[i + j + k / 2] + p) % p;
+                }
+            }
+        }
+    }
+}
+// 按位异或
+void FWT_xor(vector<int> &a, int t, int p) {
+    int n = a.size();
+    for (int k = 2; k <= n; k *= 2) {
+        for (int i = 0; i < n; i += k) {
+            for (int j = 0; j < k / 2; j++) {
+                int x = a[i + j], y = a[i + j + k / 2];
+                a[i + j] = (x + y) % p;
+                a[i + j + k / 2] = (x - y + p) % p;
             }
         }
     }
     if (t < 0) {
-        int inv = qpow(n % p, p - 2); // n 的逆元，在不取模时需要用每层除以 2 代替
+        int inv = qpow(n % p, p - 2, p); // n 的逆元，在不取模时需要用每层除以 2 代替
         for (int i = 0; i < n; i++) {
-            A[i] = A[i] * inv % p;
+            a[i] = 1ll * a[i] * inv % p;
         }
     }
 }
