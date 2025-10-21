c4484e84112d704abdb59dcbfba81d46ad420271
WSFcloud <byhtdcc@gmail.com>
2025-10-21 23:27:04 +0800

    add: 增加生成区间内随机数

--- src/杂项/随机数生成.cpp
+++ src/杂项/随机数生成.cpp
@@ -1,2 +1,6 @@
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 mt19937_64 rng_64(chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> disti(l, r);     // 生成区间 [l, r] 上的随机数
+uniform_real_distribution<double> distr(l, r); // 生成区间 [l, r) 上的随机数
+int random_int = disti(rng);
+double random_double = distr(rng);
