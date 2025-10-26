cf3e65e0af7618464cd6964ea64d16a51e809105
WSFcloud <byhtdcc@gmail.com>
2025-10-26 12:35:57 +0800

    fix: 修改二进制位操作的最低位为0

--- src/数学/二进制相关.cpp
+++ src/数学/二进制相关.cpp
@@ -1,24 +1,24 @@
 // 获取 x 的第 i 位的 bool 值
 int get_th(int x, int i) {
-    return (x >> (i - 1)) & 1;
+    return (x >> i) & 1;
 }
 // 设置 x 的第 i 位为 1
 void set_th(int &x, int i) {
-    x = x | (1 << (i - 1));
+    x = x | (1 << i);
 }
 // 设置 x 的第 i 位为 0
 void clear_th(int &x, int i) {
-    x = x & ~(1 << (i - 1));
+    x = x & ~(1 << i);
 }
 // 将 x 的第 i 位取反
 void flip_th(int &x, int i) {
-    x ^= (1 << (i - 1));
+    x ^= (1 << i);
 }
-// 取出 x 的第 1 位到第 i 位
+// 取出 x 的第 0 位到第 i 位
 int get_low(int x, int i) {
-    return x & ((1 << i) - 1);
+    return x & ((1 << (i + 1)) - 1);
 }
-// 将 x 加上 $v \times 2 ^ {i - 1}$
+// 将 x 加上 $v \times 2 ^ i$
 void make_th(int &x, int i, int v) {
-    x = x + (v << (i - 1));
+    x = x + (v << i);
 }
