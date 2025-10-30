fdf5daa9a25e5abc6ce2bdd3fcd09d946144c0e2
WSFcloud <byhtdcc@gmail.com>
2025-10-31 00:04:16 +0800

    add: 2-SAT

---
+++ src/图论/2-SAT.cpp
@@ -0,0 +1,52 @@
+struct TwoSat {
+    int n;
+    vector<vector<int>> e;
+    vector<bool> ans; // 0 base
+    TwoSat(int n_) : n(n_), e(2 * n_), ans(n_) {}
+    // 添加子句 (f)u or (g)v
+    void add_clause(int u, bool f, int v, bool g) {
+        e[2 * u + !f].push_back(2 * v + g);
+        e[2 * v + !g].push_back(2 * u + f);
+    }
+    bool satisfiable() {
+        vector<int> id(2 * n, -1), dfn(2 * n, -1), low(2 * n, -1);
+        vector<int> stk;
+        int now = 0, cnt = 0;
+        auto tarjan = [&](auto self, int u) -> void {
+            stk.push_back(u);
+            dfn[u] = low[u] = now++;
+            for (auto v : e[u]) {
+                if (dfn[v] == -1) {
+                    self(self, v);
+                    low[u] = min(low[u], low[v]);
+                } else if (id[v] == -1) {
+                    low[u] = min(low[u], dfn[v]);
+                }
+            }
+            if (dfn[u] == low[u]) {
+                int v;
+                do {
+                    v = stk.back();
+                    stk.pop_back();
+                    id[v] = cnt;
+                } while (v != u);
+                ++cnt;
+            }
+        };
+        for (int i = 0; i < 2 * n; ++i) {
+            if (dfn[i] == -1) {
+                tarjan(tarjan, i);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (id[2 * i] == id[2 * i + 1]) {
+                return false;
+            }
+            ans[i] = id[2 * i] > id[2 * i + 1];
+        }
+        return true;
+    }
+    vector<bool> answer() {
+        return ans;
+    }
+};
